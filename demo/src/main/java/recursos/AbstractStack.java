0df10b122808d87821bc3a955e1adb16b7c7e8fa
KristCg <kriscastillo515@gmail.com>
2025-02-28 20:09:18 -0600

    Clase abstracta de Stack

---
+++ demo/src/main/java/recursos/AbstractStack.java
@@ -0,0 +1,17 @@
+package recursos;
+
+public abstract class AbstractStack<T> implements IStack<T>{
+    protected int size = 0;
+
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    
+}
